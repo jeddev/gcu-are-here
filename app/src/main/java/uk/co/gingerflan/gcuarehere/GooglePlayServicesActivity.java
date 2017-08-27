38ca088cf8b189d3d7d63fdf80681edfa515428f
jed <jdunca210@caledonian.ac.uk>
2017-08-27 15:38:27 +0100

    Off again

---
+++ app/src/main/java/uk/co/gingerflan/gcuarehere/GooglePlayServicesActivity.java
@@ -0,0 +1,151 @@
+package uk.co.gingerflan.gcuarehere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.location.Location;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+
+
+public class GooglePlayServicesActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
+
+
+
+{
+    protected static final int REQUEST_CODE_RESOLUTION = 1;
+    private static final String TAG = "GooglePlayServicesActiv";
+    private static final String KEY_IN_RESOLUTION = "is_in_resolution";
+    protected Location myLastLocation;
+    protected TextView myTextView;
+    protected LocationRequest myLocationRequest;
+    private GoogleApiClient mGoogleApiClient;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private boolean mIsInResolution;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+            if (savedInstanceState != null) {
+                mIsInResolution = savedInstanceState.getBoolean(KEY_IN_RESOLUTION, false);
+            }
+        setContentView(R.layout.activity_google_play_services);
+
+        myTextView = (TextView) findViewById(R.id.textView);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop(){
+            if (mGoogleApiClient !=null){
+                mGoogleApiClient.disconnect();
+            }
+            super.onStop();
+     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_IN_RESOLUTION, mIsInResolution);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case REQUEST_CODE_RESOLUTION:
+              retryConnecting();
+                break;
+        }
+    }
+
+    private void retryConnecting() {
+        mIsInResolution = false;
+        if (mGoogleApiClient.isConnecting()){
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.i(TAG, "GoogleApiClient connected");
+        myLastLocation =
+                LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (myLastLocation != null) {
+            myTextView.setText("Latitude: "+ myLastLocation.getLatitude()
+            + "\nLongtitude: " + myLastLocation.getLongitude());
+        } else {
+            Toast.makeText(this, "Location data not available",
+                    Toast.LENGTH_LONG) .show();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.i(TAG, "GoogleApiClient connection suspended");
+        retryConnecting();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG,  "GoogleApiClient connection failed: " + result.toString());
+        if (!result.hasResolution()) {
+            GooglePlayServicesUtil.getErrorDialog(
+                    result.getErrorCode(), this, 0, new OnCancelListener() {
+                @Override
+                public void onCancel (DialogInterface dialog){
+                    retryConnecting();
+                }
+            }).show();
+        return;
+    }
+    if (mIsInResolution) {
+        return;
+    }
+    mIsInResolution = true;
+        try{
+            result.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
+        } catch (SendIntentException e) {
+            Log.e(TAG, "Exception while starting resolution activity", e);
+            retryConnecting();
+        }
+}
+
+    protected void createLocationRequest() {
+        myLocationRequest = new LocationRequest();
+        myLocationRequest.setInterval(5000);
+        myLocationRequest.setFastestInterval(1000);
+        myLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+}
+
+
+
---
+++ app/src/main/java/uk/co/gingerflan/gcuarehere/MainActivity.java
@@ -0,0 +1,21 @@
+package uk.co.gingerflan.gcuarehere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick (View view) {
+        startActivity(new Intent("uk.co.gingerflan.gcuarehere.GooglePlayServicesActivity"));
+    }
+}
